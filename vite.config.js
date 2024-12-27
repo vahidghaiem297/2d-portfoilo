7ecd23145f1200801952773d765e89479e83dd1b
unknown <vahidghaiem297@gmail.com>
2024-12-27 00:53:31 -0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/2d-portfoilo/", // YOUR REPO NAME HERE
+})
